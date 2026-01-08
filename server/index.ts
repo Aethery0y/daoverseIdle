c28beed349df89788fa0623cac4fc22e112ee199
Aether <aethery0y@gmail.com>
2026-01-09 00:31:40 +0530

    update

--- server/index.ts
+++ server/index.ts
@@ -112,7 +112,13 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+  // ALWAYS serve the app on the port specified in the environment variable PORT
+  // Other ports are firewalled. Default to 5000 if not specified.
+  const port =
+    parseInt(process.env.PORT || "", 10) ||
+    Number(process.argv.find((a) => a.startsWith("--port="))?.split("=")[1]) ||
+    5000;
+
   httpServer.listen(
     {
       port,
